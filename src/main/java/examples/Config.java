bfb7acea241b49b482c126c06676283b0b701e60
GideonLeGrange <gideon@legrange.me>
2013-07-11 16:56:22 +0200

    Added some examples, more to follow

---
+++ src/main/java/examples/Config.java
@@ -0,0 +1,18 @@
+package examples;
+
+/**
+ * Config class for examples
+ * @author gideon
+ */
+public class Config {
+    
+    public static final String USERNAME = "adept";
+    public static final String PASSWORD = "tzrx21j";
+    public static final String HOST = "196.44.35.227";
+    
+/*    public static final String USERNAME = "admin";
+    public static final String PASSWORD = "";
+    public static final String HOST = "10.0.0.1";
+    
+  */  
+}
