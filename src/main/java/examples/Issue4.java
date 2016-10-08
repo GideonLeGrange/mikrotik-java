a6230a82bff891b406c34aee475ff32506ab12e4
Gideon le Grange <gideon@legrange.me>
2016-10-08 19:05:09 +0200

    Fix #40. Changed string encoding to UTF8 when sending bytes accross the network

---
+++ src/main/java/examples/Issue4.java
@@ -0,0 +1,30 @@
+package examples;
+
+import java.util.List;
+import java.util.Map;
+import me.legrange.mikrotik.MikrotikApiException;
+
+/**
+ * Example 7: Dump your complete config
+ *
+ * @author gideon
+ */
+public class DownloadConfig extends Example {
+
+    public static void main(String... args) throws Exception {
+        DownloadConfig ex = new DownloadConfig();
+        ex.connect();
+        ex.test();
+        ex.disconnect();
+    }
+
+    private void test() throws MikrotikApiException, InterruptedException {
+        con.execute("/export file=conf");
+        List<Map<String, String>> res = con.execute("/file/print detail where name=conf.rsc");
+        con.execute("/file/remove .id=conf.rsc");
+        String text = res.get(0).get("contents");
+        for (String line : text.split("\r")) {
+            System.out.println(line);
+        }
+    }
+}
