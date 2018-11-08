81efcd6c73dddbb5219f8a898639bdb0ea34a593
Gideon le Grange <gideon@legrange.me>
2018-11-08 06:30:58 +0200

    Implemented support for new login method used from 6.43

--- src/main/java/me/legrange/mikrotik/impl/ApiConnectionImpl.java
+++ src/main/java/me/legrange/mikrotik/impl/ApiConnectionImpl.java
@@ -56,12 +56,14 @@ public final class ApiConnectionImpl extends ApiConnection {
             throw new ApiConnectionException("API username cannot be empty");
         }
         List<Map<String, String>> list = execute("/login name=" + username + " password=" + password);
-        Map<String, String> res = list.get(0);
-        if (res.containsKey("ret")) {
-            String hash = res.get("ret");
-            String chal = Util.hexStrToStr("00") + new String(password.toCharArray()) + Util.hexStrToStr(hash);
-            chal = Util.hashMD5(chal);
-            execute("/login name=" + username + " response=00" + chal);
+        if (!list.isEmpty()) {
+            Map<String, String> res = list.get(0);
+            if (res.containsKey("ret")) {
+                String hash = res.get("ret");
+                String chal = Util.hexStrToStr("00") + new String(password.toCharArray()) + Util.hexStrToStr(hash);
+                chal = Util.hashMD5(chal);
+                execute("/login name=" + username + " response=00" + chal);
+            }
         }
     }
 
